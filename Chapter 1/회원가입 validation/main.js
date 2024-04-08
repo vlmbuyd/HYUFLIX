23fe359a3c872163d94a7b85784396e64a0b4d9f
Yean <kjan0929@naver.com>
2024-04-08 11:48:48 +0900

    Validation whether writen or not

--- Chapter 1/회원가입 validation/main.js
+++ Chapter 1/회원가입 validation/main.js
@@ -1,16 +1,54 @@
-const userName = document.getElementById("name");
+const inputName = document.getElementById("input-name");
+const inputEmail = document.getElementById("input-email");
+const inputAge = document.getElementById("input-age");
+const inputPw = document.getElementById("input-pw");
+const inputPwCheck = document.getElementById("input-pw.check");
 
-const signUp = document.getElementById("sign-up");
+const message = document.querySelectorAll(".message");
+const nameMessage = document.getElementById("name-message");
+const emailMessage = document.getElementById("email-message");
+const ageMessage = document.getElementById("age-message");
+const pwMessage = document.getElementById("pw-message");
+const pwCheckMessage = document.getElementById("pw-check-message");
 
-const nameHandler = () => {
-  if (isNaN(userName.value)) {
-    //문자열이 아닐 때
-    const p = document.createElement("p");
-    p.innerText = "올바른 형식이 아닙니다!";
+const signUpBtn = document.getElementById("sign-up-btn");
+
+const writeValidation = () => {
+  //하나라도 작성 안되면 전체가 오류메시지 뜨는 상황
+  if (
+    inputName.value == "" ||
+    inputEmail.value == "" ||
+    inputAge.value == "" ||
+    inputPW.value == "" ||
+    inputPwCheck.value == ""
+  ) {
+    signUpBtn.disabled = false;
+
+    message.forEach((message) => {
+      message.classList.add("fail-message");
+      message.classList.remove("hide");
+    });
+
+    nameMessage.innerText = "필수 입력 항목입니다!";
+    emailMessage.innerText = "올바른 이메일 형식이 아닙니다!";
+    ageMessage.innerText = "나이를 입력해주세요!";
+    pwMessage.innerText = "비밀번호는 최소 4자리 이상이어야 합니다.";
+    pwCheckMessage.innerText = "비밀번호가 일치하지 않습니다.";
   }
 };
 
-signUp.addEventListener("submit", () => {
-  console.log(userName);
-  nameHandler();
-});
+const nameHandler = () => {
+  const reg = /^[가-힣a-zA-Z]$/;
+
+  console.log(inputName.value);
+
+  if (reg.test(inputName.value)) {
+    //문자열 작성 맞을 때
+    nameMessage.classList.remove("hide");
+    nameMessage.innerText = "멋진 이름이에요!";
+  } else {
+    nameMessage.innerText = "필수 입력항목입니다!";
+  }
+};
+
+signUpBtn.addEventListener("click", writeValidation);
