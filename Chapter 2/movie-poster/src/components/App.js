6b5d24c3c4280bf8aad36b3c6c809934cb860927
Yean <kjan0929@naver.com>
2024-05-11 17:30:24 +0900

    stying

--- Chapter 2/movie-poster/src/components/App.js
+++ Chapter 2/movie-poster/src/components/App.js
@@ -1,11 +1,35 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import MovieList from "./MovieList";
 import { mockData } from "../mock";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
+
+const Container = styled.div`
+  width: 80%;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #242a6b;
+`;
 
 function App() {
   const mock = mockData.results;
 
   console.log(mock);
-  return <MovieList items={mock}></MovieList>;
+  return (
+    <Container>
+      <GlobalStyle></GlobalStyle>
+      <MovieList items={mock}></MovieList>
+    </Container>
+  );
 }
 
 export default App;
--- Chapter 2/movie-poster/src/components/MovieList.js
+++ Chapter 2/movie-poster/src/components/MovieList.js
@@ -1,6 +1,17 @@
+import styled from "styled-components";
+import MovieListItem from "./MovieListItem";
+
+const Ul = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  list-style: none;
+  padding-left: 0;
+`;
+
 function MovieList({ items }) {
   return (
-    <ul>
+    <Ul>
       {items.map((item) => {
         return (
           <li key={item.id}>
@@ -8,19 +19,7 @@ function MovieList({ items }) {
           </li>
         );
       })}
-    </ul>
-  );
-}
-
-function MovieListItem({ item }) {
-  return (
-    <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
-        alt={item.original_title}
-      ></img>
-      <h3>{item.original_title}</h3>
-    </div>
+    </Ul>
   );
 }
 
