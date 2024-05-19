10695cef15d9f6211b435aad3dc0eb3f37620d01
Yean <kjan0929@naver.com>
2024-05-20 01:21:06 +0900

    movie detail description

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import getAPI from "../api/api";
+import Styled from "../styles/moviedetail";
+
+function MovieDetail({ index }) {
+  const [item, setItem] = useState({});
+  const { title } = useParams();
+
+  function getDetail(arr, title) {
+    return arr.find((item) => item.original_title === title);
+  }
+
+  const handleDetail = async () => {
+    const detailData = await getAPI(index);
+    const data = detailData.results;
+    const detail = getDetail(data, title);
+    console.log(detail);
+    setItem(detail);
+  };
+
+  useEffect(() => {
+    handleDetail();
+  }, [title]);
+
+  return (
+    <Styled.Container>
+      <Styled.BackgroundImg
+        src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
+        alt={item.original_title}
+      />
+      <img
+        className="movie-img"
+        src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
+        alt={item.original_title}
+      />
+      <div className="description">
+        <h1 className="title">{item.original_title}</h1>
+        <strong className="rating">평점 {item.vote_average}</strong>
+        <strong className="release-date">개봉일 {item.release_date}</strong>
+        <strong className="plot-title">줄거리</strong>
+        <p className="plot">{item.overview}</p>
+      </div>
+    </Styled.Container>
+  );
+}
+
+export default MovieDetail;
---
+++ src/styles/moviedetail.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+
+  & .movie-img {
+    width: 300px;
+    height: 420px;
+    position: absolute;
+    top: 15%;
+    left: 15%;
+    object-fit: cover;
+  }
+
+  & .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    position: absolute;
+    top: 20%;
+    left: 40%;
+    width: 500px;
+    height: 300px;
+    color: white;
+  }
+
+  & .title {
+    font-size: 25px;
+  }
+
+  & .rating,
+  .release-date,
+  .plot-title {
+    font-size: 15px;
+  }
+
+  & .plot {
+    font-size: 13px;
+    line-height: 1.5em;
+  }
+`;
+
+const BackgroundImg = styled.img`
+  width: 100%;
+  height: 91vh;
+  object-fit: cover;
+  opacity: 0.2;
+`;
+
+export default { Container, BackgroundImg };
